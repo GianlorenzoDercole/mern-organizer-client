74ceb1bccba409ab653d92f3d8eeeebe2779a28a
GianlorenzoDercole <johndercole01@gmail.com>
2022-11-07 12:55:51 -0800

    added task links to home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-
+import { Link } from 'react-router-dom'
 export default function Home() {
+    // tasks from the backend
+    const [tasks, setTasks] = useState([])
 
     useEffect(() => {
         const fetchTasks = async () => {
             try{
                 const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/tasks`)
+                setTasks(response.data)
                 console.log(response.data)
             } catch (err) {
                 console.log(err)
@@ -14,9 +17,17 @@ export default function Home() {
         }
         fetchTasks()
     }, [])
+    const taskLinks = tasks.map((task, idx) => {
+        return (
+            <div key={`tasklink${idx}`}>
+                <Link to={`/tasks/${task._id}`}>{task.item}</Link>
+            </div>
+        )
+    })
     return (
         <div>
             i am the home component
+            {taskLinks}
         </div>
     )
 }
