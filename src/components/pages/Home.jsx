17ff9b91eb54bc19277732c738e5f4e620141a88
GianlorenzoDercole <johndercole01@gmail.com>
2022-11-07 12:40:03 -0800

    connected to server

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,19 @@
-
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            try{
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/tasks`)
+                console.log(response.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchTasks()
+    }, [])
     return (
         <div>
             i am the home component
