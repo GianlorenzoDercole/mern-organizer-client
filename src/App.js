2fdb718c6f16f5630eb2bb3babdfc123cbfa4781
GianlorenzoDercole <johndercole01@gmail.com>
2022-11-07 12:09:39 -0800

    app.js is connected to home and task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Home from './components/pages/Home'
+import Task from './components/pages/Task'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div >
+    <Router>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />}/>
 
-    </div>
+          <Route path='/tasks/:id' element={<Task />}/>
+
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Home() {
+    return (
+        <div>
+            i am the home component
+        </div>
+    )
+}
