493d1ab829af57bc095cf77118ff479704dd9094
GianlorenzoDercole <johndercole01@gmail.com>
2022-11-08 15:36:11 -0800

    added delete button see task.jsx

--- src/components/pages/Task.jsx
+++ src/components/pages/Task.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import TaskDetails from '../TaskDetails'
@@ -7,7 +7,7 @@ export default function Task() {
     const [task, setTask] = useState({})
     const [showForm, setShowForm] = useState(false)
     const { id } = useParams()
-
+    const navigate = useNavigate()
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_SERVER_URL}/tasks/${id}`)
             .then(response => {
@@ -26,11 +26,20 @@ export default function Task() {
             })
             .catch(console.warn)
     }
+    const handleDelete = () => {
+        axios.delete(`${process.env.REACT_APP_SERVER_URL}/tasks/${id}`)
+            .then(response => {
+                // navigate away from this page
+                navigate('/')
+            })
+            .catch(console.warn)
+    }
     return (
         <div>
-            
+
             { showForm ? <TaskForm initialForm={task} submitHandler={handleSubmit}/> : <TaskDetails task={task}/> }
             <button onClick={() => setShowForm(!showForm)}>{showForm ? 'no' : 'edit'}</button>
+            {showForm ? <button onClick={handleDelete}>dleete</button> : ''}
         </div>
     )
 }
